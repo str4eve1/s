0d44385d46193afc669186c93dc4bf3d872e66b8
Peter Hutterer <peter.hutterer@who-t.net>
2017-07-27 21:24:10 +1000

    Revert "data: move ui files into subdirs"
    This reverts commit a0a76dbf7b9e436e68f8978d604c62dfcc552a52.

--- piper/resolutionrow.py
+++ piper/resolutionrow.py
@@ -22,7 +22,7 @@ gi.require_version("Gtk", "3.0")
 from gi.repository import GObject, Gtk
 
 
-@GtkTemplate(ui="/org/freedesktop/Piper/ui/ResolutionRow.ui")
+@GtkTemplate(ui="/org/freedesktop/Piper/resolutionRow.ui")
 class ResolutionRow(Gtk.ListBoxRow):
     """A Gtk.ListBoxRow subclass containing the widgets to configure a
     resolution."""
