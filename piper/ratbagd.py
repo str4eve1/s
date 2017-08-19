84abae97fe3e208e6f23d8c0f6d2afe5f8b740a2
Jente Hidskes <hjdskes@gmail.com>
2017-08-19 21:34:03 +0200

    Ratbagd: _set_dbus_property doesn't return anything
    We don't want to return values from setters, but more importantly,
    _set_dbus_property doesn't even return anything anymore.

--- piper/ratbagd.py
+++ piper/ratbagd.py
@@ -398,7 +398,7 @@ class RatbagdProfile(_RatbagdDBus):
         """Enable/Disable this profile.
 
         @param enabled The new state, as boolean"""
-        return self._set_dbus_property("Enabled", "b", enabled)
+        self._set_dbus_property("Enabled", "b", enabled)
 
     @GObject.Property
     def resolutions(self):
@@ -481,7 +481,7 @@ class RatbagdResolution(_RatbagdDBus):
 
         @param res The new resolution, as (int, int)
         """
-        return self._set_dbus_property("Resolution", "(uu)", res)
+        self._set_dbus_property("Resolution", "(uu)", res)
 
     @GObject.Property
     def report_rate(self):
@@ -494,7 +494,7 @@ class RatbagdResolution(_RatbagdDBus):
 
         @param rate The new report rate, as int
         """
-        return self._set_dbus_property("ReportRate", "u", rate)
+        self._set_dbus_property("ReportRate", "u", rate)
 
     @GObject.Property
     def maximum(self):
@@ -759,7 +759,7 @@ class RatbagdLed(_RatbagdDBus):
         @param mode The new mode, as one of MODE_OFF, MODE_ON, MODE_CYCLE and
                     MODE_BREATHING.
         """
-        return self._set_dbus_property("Mode", "u", mode)
+        self._set_dbus_property("Mode", "u", mode)
 
     @GObject.Property
     def type(self):
@@ -778,7 +778,7 @@ class RatbagdLed(_RatbagdDBus):
 
         @param color An RGB color, as an integer triplet with values 0-255.
         """
-        return self._set_dbus_property("Color", "(uuu)", color)
+        self._set_dbus_property("Color", "(uuu)", color)
 
     @GObject.Property
     def effect_rate(self):
@@ -791,7 +791,7 @@ class RatbagdLed(_RatbagdDBus):
 
         @param effect_rate The new effect rate, as int
         """
-        return self._set_dbus_property("EffectRate", "u", effect_rate)
+        self._set_dbus_property("EffectRate", "u", effect_rate)
 
     @GObject.Property
     def brightness(self):
@@ -804,4 +804,4 @@ class RatbagdLed(_RatbagdDBus):
 
         @param brightness The new brightness, as int
         """
-        return self._set_dbus_property("Brightness", "u", brightness)
+        self._set_dbus_property("Brightness", "u", brightness)
