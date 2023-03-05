str4eve1/s

commit d390f22210bd317a58bea1996df6c0b3aba58b3c

ratbagd.py: sync with Piper
Make RatbagdProfile more reliable and fix incorrect value on start.

piper/ratbagd.py
@@ -407,10 +407,6 @@ class RatbagdDevice(_RatbagdDBus):
         device. No further interaction is required by the client.
         """
         self._dbus_call("Commit", "")
-        for profile in self._profiles:
-            if profile.dirty:
-                profile._dirty = False
-                profile.notify("dirty")
 
 
 class RatbagdProfile(_RatbagdDBus):
@@ -423,32 +419,20 @@ class RatbagdProfile(_RatbagdDBus):
 
     def __init__(self, object_path):
         super().__init__("Profile", object_path)
-        self._dirty = False
         self._active = self._get_dbus_property("IsActive")
+        self._dirty = self._get_dbus_property("IsDirty")
         self._report_rate = self._get_dbus_property("ReportRate")
 
         # FIXME: if we start adding and removing objects from any of these
         # lists, things will break!
         result = self._get_dbus_property("Resolutions") or []
         self._resolutions = [RatbagdResolution(objpath) for objpath in result]
-        self._subscribe_dirty(self._resolutions)
 
         result = self._get_dbus_property("Buttons") or []
         self._buttons = [RatbagdButton(objpath) for objpath in result]
-        self._subscribe_dirty(self._buttons)
 
         result = self._get_dbus_property("Leds") or []
         self._leds = [RatbagdLed(objpath) for objpath in result]
-        self._subscribe_dirty(self._leds)
-
-    def _subscribe_dirty(self, objects):
-        for obj in objects:
-            obj.connect("notify", self._on_obj_notify)
-
-    def _on_obj_notify(self, obj, pspec):
-        if not self._dirty:
-            self._dirty = True
-            self.notify("dirty")
 
     def _on_properties_changed(self, proxy, changed_props, invalidated_props):
         if "IsActive" in changed_props.keys():
@@ -456,13 +440,12 @@ class RatbagdProfile(_RatbagdDBus):
             if active != self._active:
                 self._active = active
                 self.notify("is-active")
-                self._on_obj_notify(None, None)
 
-        if "ReportRate" in changed_props.keys():
-            report_rate = changed_props["ReportRate"]
-            if report_rate != self._report_rate:
-                self._report_rate = report_rate
-                self._on_obj_notify(None, None)
+        if "IsDirty" in changed_props.keys():
+            dirty = changed_props["IsDirty"]
+            if dirty != self._dirty:
+                self._dirty = dirty
+                self.notify("dirty")
 
     @GObject.Property
     def capabilities(self):
@@ -538,7 +521,6 @@ class RatbagdProfile(_RatbagdDBus):
         @param value The angle snapping option as int
         """
         self._set_dbus_property("AngleSnapping", "i", value)
-        self._on_obj_notify(None, None)
 
     @GObject.Property
     def debounce(self):
@@ -552,7 +534,6 @@ class RatbagdProfile(_RatbagdDBus):
         @param value The button debounce time, as int
         """
         self._set_dbus_property("Debounce", "i", value)
-        self._on_obj_notify(None, None)
 
     @GObject.Property
     def debounces(self):

commit 12109d6f61edaf72da3bbd5abaa0a45f0f23478e

ratbagd.py: connect to notify::dirty of child objects in the profile
This fixes regression of commit button not becoming active introduced by
me in d390f22210bd317a58bea1996df6c0b3aba58b3c.

piper/ratbagd.py
@@ -27,6 +27,7 @@ from enum import IntEnum
 from evdev import ecodes
 from gettext import gettext as _
 from gi.repository import Gio, GLib, GObject
+from typing import List, Optional
 
 
 # Deferred translations, see https://docs.python.org/3/library/gettext.html#deferred-translations
@@ -427,12 +428,24 @@ class RatbagdProfile(_RatbagdDBus):
         # lists, things will break!
         result = self._get_dbus_property("Resolutions") or []
         self._resolutions = [RatbagdResolution(objpath) for objpath in result]
+        self._subscribe_dirty(self._resolutions)
 
         result = self._get_dbus_property("Buttons") or []
         self._buttons = [RatbagdButton(objpath) for objpath in result]
+        self._subscribe_dirty(self._buttons)
 
         result = self._get_dbus_property("Leds") or []
         self._leds = [RatbagdLed(objpath) for objpath in result]
+        self._subscribe_dirty(self._leds)
+
+    def _subscribe_dirty(self, objects: List[GObject.GObject]):
+        for obj in objects:
+            obj.connect("notify", self._on_obj_notify)
+
+    def _on_obj_notify(self, obj: GObject.GObject, pspec: Optional[GObject.ParamSpec]):
+        if not self._dirty:
+            self._dirty = True
+            self.notify("dirty")
 
     def _on_properties_changed(self, proxy, changed_props, invalidated_props):
         if "IsActive" in changed_props.keys():